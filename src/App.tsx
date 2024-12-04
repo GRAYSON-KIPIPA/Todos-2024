8b84bfcca5dfe6dc9203338c4446ec8905ba1520
grayson-kipipa <mapundagrayson@gmail.com>
2024-12-04 10:39:26 +0300

    the project builded

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,8 @@
-import { useState } from "react";
-import Dashboard from "./components/Dashboard";
-import Examples from "./components/Examples";
-import RegisterStudent from "./components/RegisterStudent";
 import TodoApp from "./components/TodoApp";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      {/* <RegisterStudent /> */}
-      {/* <Dashboard />    
-       <Examples /> */}
-
       <TodoApp />
     </>
   );
